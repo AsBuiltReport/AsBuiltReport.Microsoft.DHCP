ff368050b27f286ef58977091e7678f036e4b961
Jonathan Colon <rebelinux@gmail.com>
2023-05-23 11:38:35 -0400

    Add Server Scope Class section #6

--- Src/Private/Get-AbrADDHCPv6Scope.ps1
+++ Src/Private/Get-AbrADDHCPv6Scope.ps1
@@ -33,7 +33,7 @@ function Get-AbrADDHCPv6Scope {
         $DHCPScopes = Get-DhcpServerv6Scope -CimSession $TempCIMSession -ComputerName $Server
         if ($DHCPScopes) {
             Section -Style Heading4 "Scopes" {
-                Paragraph "The following sections detail the configuration of the ipv4 scope within $($Server.ToUpper().split(".", 2)[0])."
+                Paragraph "The following sections detail the configuration of the ipv6 scope within $($Server.ToUpper().split(".", 2)[0])."
                 BlankLine
                 $OutObj = @()
                 foreach ($Scope in $DHCPScopes) {
@@ -145,6 +145,84 @@ function Get-AbrADDHCPv6Scope {
             }
             catch {
                 Write-PscriboMessage -IsWarning "$($_.Exception.Message) (IPv6 Network Interface binding table)"
+            }
+            try {
+                $DHCPClass = Get-DhcpServerv6Class -CimSession $TempCIMSession -ComputerName $Server | Sort-Object -Property 'Name'
+                if ($DHCPClass) {
+                    Section -Style Heading4 "Client Classes" {
+                        $OutObj = @()
+                        foreach ($Class in $DHCPClass) {
+                            try {
+                                Write-PscriboMessage "Collecting DHCP Server IPv6 $($Class.Name) class from $($Server.split(".", 2)[0])"
+                                $inObj = [ordered] @{
+                                    'Name' = $Class.Name
+                                    'Type' = $Class.Type
+                                    'Data' = $Class.Data
+                                    'Ascii Data' = $Class.AsciiData
+                                    'Description' = $Class.Description
+                                }
+                                $OutObj += [pscustomobject]$inobj
+                            }
+                            catch {
+                                Write-PscriboMessage -IsWarning "$($_.Exception.Message) (IPv6 Client Classes Item)"
+                            }
+                        }
+
+                        if ($HealthCheck.DHCP.BP) {
+                            $OutObj | Where-Object { $Null -eq $_.'Description'} | Set-Style -Style Warning -Property 'Description'
+                        }
+
+                        $TableParams = @{
+                            Name = "Client Classes - $($Server.split(".", 2).ToUpper()[0])"
+                            List = $false
+                            ColumnWidths = 24, 12, 24, 20, 20
+                        }
+                        if ($Report.ShowTableCaptions) {
+                            $TableParams['Caption'] = "- $($TableParams.Name)"
+                        }
+                        $OutObj | Table @TableParams
+                    }
+                }
+            }
+            catch {
+                Write-PscriboMessage -IsWarning "$($_.Exception.Message) (IPv6 Client Classes Table)"
+            }
+            try {
+                $DHCPOptionDefinition = Get-DhcpServerv6OptionDefinition -CimSession $TempCIMSession -ComputerName $Server | Sort-Object -Property 'OptionId'
+                if ($DHCPOptionDefinition) {
+                    Section -Style Heading4 "Option Definitions" {
+                        $OutObj = @()
+                        foreach ($Definition in $DHCPOptionDefinition) {
+                            try {
+                                Write-PscriboMessage "Collecting DHCP Server IPv6 $($Definition.Name) option definitions from $($Server.split(".", 2)[0])"
+                                $inObj = [ordered] @{
+                                    'Name' = $Definition.Name
+                                    'Option Id' = $Definition.OptionId
+                                    'Type' = $Definition.Type
+                                    'Vendor Class' = ConvertTo-EmptyToFiller $Definition.VendorClass
+                                    'Multi Valued' = ConvertTo-TextYN $Definition.MultiValued
+                                }
+                                $OutObj += [pscustomobject]$inobj
+                            }
+                            catch {
+                                Write-PscriboMessage -IsWarning "$($_.Exception.Message) (IPv6 Option Definitions Item)"
+                            }
+                        }
+
+                        $TableParams = @{
+                            Name = "Option Definitions - $($Server.split(".", 2).ToUpper()[0])"
+                            List = $false
+                            ColumnWidths = 30, 12, 22, 22, 14
+                        }
+                        if ($Report.ShowTableCaptions) {
+                            $TableParams['Caption'] = "- $($TableParams.Name)"
+                        }
+                        $OutObj | Table @TableParams
+                    }
+                }
+            }
+            catch {
+                Write-PscriboMessage -IsWarning "$($_.Exception.Message) (IPv6 Client Classes Table)"
             }
         }
     }
